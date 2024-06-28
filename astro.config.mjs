18090040a9f72f31dd31c8d85c653897ab0b8a04
Steven Wolf <steven.wolf+git@pm.me>
2024-06-28 11:16:14 +0200

    add amplify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import awsAmplify from "astro-aws-amplify";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "middleware",
-  }),
+  adapter: awsAmplify(),
   integrations: [tailwind(), svelte()],
   devToolbar: {
     enabled: false,
