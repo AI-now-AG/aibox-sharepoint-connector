faa9560e960995832d407c407d4802aed414b361
Steven Wolf <steven.wolf+git@pm.me>
2024-07-30 14:00:59 +0200

    fix typing, ensure lang exists

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,12 @@
 import { ui, defaultLang } from "./ui";
 
-export function useTranslations(lang?: string) {
+const getLang = (s?: string) => {
+  if (s && s in ui) return s as keyof typeof ui;
+};
+
+export function useTranslations(requestedLang?: string) {
+  const lang = getLang(requestedLang);
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return (lang && ui[lang][key]) || ui[defaultLang][key];
+    return (lang && ui[lang][key]) || ui[defaultLang][key] || "";
   };
 }
