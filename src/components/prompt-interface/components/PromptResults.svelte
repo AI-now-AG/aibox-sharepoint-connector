4c0ee2cbf257f15b7a88fede60799ef71550b8be
Steven <jlwolfi@gmail.com>
2024-08-29 16:13:05 +0200

    hide input

--- src/components/prompt-interface/components/PromptResults.svelte
+++ src/components/prompt-interface/components/PromptResults.svelte
@@ -10,11 +10,11 @@
     <div class="grow md:w-1/2 p-2 pb-4">
       {#if input}
         <div class="grid space-y-6" transition:fade>
-          <div class="card grid justify-items-end mt-2">
+          <!--<div class="card grid justify-items-end mt-2">
             <div class="chat-bubble bg-neutral-content text-info-content">
               {input}
             </div>
-          </div>
+          </div>-->
           <Output bind:output />
         </div>
       {/if}
