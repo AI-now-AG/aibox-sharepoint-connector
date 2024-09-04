b0b25284bb724e2bbf81e0e4f109579edab91ec3
Steven <jlwolfi@gmail.com>
2024-09-04 15:47:48 +0200

    add delete confirmation dlg

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -15,6 +15,12 @@
   export let title: string = "";
   export let viewLabel: string = "";
 
+  const showDeleteConfirmationDlg = (id: string) => {
+    document
+      .querySelector<HTMLDialogElement>(`#delete_confirmation_modal_${id}`)
+      ?.showModal();
+  };
+
   async function deleteCard(id: string) {
     const response = await fetch(`/api/${type}.json`, {
       method: "DELETE",
@@ -58,16 +64,66 @@
           {/if}
           <div class="flex justify-between mt-4">
             <div class="card-actions">
-              <a href={`${type}/${card.id}`} class="btn btn-primary font-light">
+              <a
+                href={`${type}/${card.id}`}
+                class="btn btn-primary font-normal"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  class="w-6 h-6"
+                  ><path
+                    fill="currentColor"
+                    d="M3 21v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15t.775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM17.6 7.8L19 6.4L17.6 5l-1.4 1.4z"
+                  /></svg
+                >
                 {viewLabel}
               </a>
             </div>
             <button
-              class="btn btn-link btn-xs font-normal text-accent-content self-end"
-              on:click={() => deleteCard(card.id)}
+              class="btn btn-sm btn-ghost text-error self-end"
+              on:click={() => {
+                showDeleteConfirmationDlg(card.id);
+              }}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+                class="w-6 h-6"
+                ><path
+                  fill="currentColor"
+                  d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zm2-4h2V8H9zm4 0h2V8h-2z"
+                /></svg
+              >
               Delete
             </button>
+
+            <dialog id={`delete_confirmation_modal_${card.id}`} class="modal">
+              <div class="modal-box">
+                <h3 class="text-lg font-bold">Please confirm</h3>
+                <p class="py-4">
+                  Are you sure you want to delete <span class="font-bold"
+                    >{card.title}</span
+                  >?
+                </p>
+                <div class="modal-action">
+                  <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Cancel</button>
+                    <button
+                      class="btn btn-sm btn-ghost text-error self-end"
+                      on:click={() => deleteCard(card.id)}
+                    >
+                      Delete</button
+                    >
+                  </form>
+                </div>
+              </div>
+            </dialog>
           </div>
         </div>
       </div>
