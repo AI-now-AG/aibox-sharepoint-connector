bf30a3381beb1d0ed5211b0c28b9b5c19aeb53c2
Steven <jlwolfi@gmail.com>
2024-09-09 18:38:48 +0200

    video

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const aiboxCollection = defineCollection({
       lead: z.string(),
       image: image(),
       imageAlt: z.string(),
+      youtube: z.string().optional(),
       tags: z.array(z.string()).optional(),
     }),
 });
