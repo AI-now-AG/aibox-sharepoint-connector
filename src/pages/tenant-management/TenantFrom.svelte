591a5a814a29dbd2396da807254142619f9b6ed9
Steve Nguyen <steve.nguyen@business4you.ch>
2024-10-07 17:02:06 +0700

    feat: AINOW-436 format code

--- src/pages/tenant-management/TenantFrom.svelte
+++ src/pages/tenant-management/TenantFrom.svelte
@@ -1,17 +1,11 @@
 <script>
-import {
-    svgIcons
-} from "$assets/icons";
-import {
-    useTranslations
-} from "$i18n/utils";
-export let preferredLocale;
-const t = useTranslations(preferredLocale);
+  import { svgIcons } from "$assets/icons";
+  import { useTranslations } from "$i18n/utils";
+  export let preferredLocale;
+  const t = useTranslations(preferredLocale);
 </script>
+
+<div class="container max-w-full mx-auto p-6" style="font-family: Inter;"></div>
 
 <style>
 </style>
-
-<div class="container max-w-full mx-auto p-6" style="font-family: Inter;">
-
-</div>
