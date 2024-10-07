41bd81982d8c310be17992b76403de882a94d067
Steve Nguyen <steve.nguyen@business4you.ch>
2024-10-07 15:18:37 +0700

    feat: AINOW-436 [FE] [List] Implement UI - Table for List of Tenant - remove pagination

--- src/pages/tenant-management/TenantList.svelte
+++ src/pages/tenant-management/TenantList.svelte
@@ -199,11 +199,4 @@ const onSearchTenant = (keyword) => {
 
                 </div>
 
-                <div class="flex justify-center join mt-6">
-                    <button class="join-item btn btn-sm">1</button>
-                    <button class="join-item btn btn-sm btn-active">2</button>
-                    <button class="join-item btn btn-sm">3</button>
-                    <button class="join-item btn btn-sm">4</button>
-                </div>
-
                 </div>
