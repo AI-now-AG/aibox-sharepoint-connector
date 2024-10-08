b44625a5f1d49b63cad896208a5b48254d58ebd7
Steve Nguyen <hoanghcmus@gmail.com>
2024-10-08 15:51:23 +0700

    refactor: AINOW-327 clean up code

--- src/pages/tenant-management/TenantFrom.svelte
+++ src/pages/tenant-management/TenantFrom.svelte
@@ -1,13 +1,12 @@
 <script>
   import { svgIcons } from "$assets/icons";
   import { useTranslations } from "$i18n/utils";
-  export let preferredLocale;
-  export let tenant
-  const t = useTranslations(preferredLocale);
+  export let tenant;
+  const t = useTranslations();
 </script>
 
 <div class="container max-w-full mx-auto p-6" style="font-family: Inter;">
-    {JSON.stringify(tenant)}
+  {JSON.stringify(tenant)}
 </div>
 
 <style>
