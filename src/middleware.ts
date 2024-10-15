51b3b1d503fd9014267af058e51fe2962a3c5772
Devlin <tinhocgocong@gmail.com>
2024-10-15 15:26:53 +0700

    [Fix] Test inactive tenant

--- src/middleware.ts
+++ src/middleware.ts
@@ -84,7 +84,7 @@ async function restrictAccess(context: APIContext, next: MiddlewareNext) {
   // Prevent archived tenant member login
   if (
     context.locals.tenant?.active == false &&
-    context.url.pathname !== "/api/logout"
+    context.url.pathname != "/api/logout"
   ) {
     return context.redirect(`/api/logout`);
   }
