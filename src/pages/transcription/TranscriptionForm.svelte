32b8b730edd322eaa0f52ac1d318bec189fde21d
Steve Nguyen <steve.nguyen@business4you.ch>
2024-10-17 14:10:53 +0700

    test build

--- src/pages/transcription/TranscriptionForm.svelte
+++ src/pages/transcription/TranscriptionForm.svelte
@@ -1,22 +1,16 @@
-<script>
+<script lang="ts">
   import { useTranslations } from "$i18n/utils";
   import { svgIcons } from "$assets/icons";
   const t = useTranslations();
-  let isDragging = false;
-  let isUploading = false;
-  let fileName = ""
-  let fileSize = ""
 
-  function handleDrop(event) {
-    event.preventDefault();
-    isDragging = false;
+  let isDragOver = false;
 
-    const eventTarget = event.target;
-    const attachedFile = eventTarget.files[0];
-    console.log(attachedFile);
-    fileName = attachedFile.name
-    console.log(fileName);
-    // isUploading = true
+  function addFiles(
+    event: Event & { currentTarget: EventTarget & HTMLInputElement },
+  ) {
+    const eventTarget = event.target as HTMLInputElement;
+    const attachedFiles = eventTarget.files[0];
+    console.log(attachedFiles);
   }
 </script>
 
@@ -25,32 +19,38 @@
   <p>1. {t("transcription.select-transcription-model")}</p>
 
   <p class="mt-16 mb-2">2. {t("transcription.upload-video-or-audio-file")}</p>
-  <div
-    class={`relative p-8 border-2 border-dotted rounded-lg text-center 
-      ${isDragging ? "border-blue-500" : "border-gray-300"} ${isUploading ? "hidden" : "block"}`}
-  
-  >
-    <input
-      type="file"
-      multiple={false}
-      class="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
-      
-    />
-    <div class="flex flex-col items-center">
-      {@html svgIcons.upload}
-      <p class="text-base font-semibold">
-        {@html t("transcription.input-file-upload-description")}
-      </p>
-      <p class="text-sm text-gray-500 mt-1">
-        {t("transcription.supportted-file-extensions")}
-      </p>
-      <p class="text-xs text-gray-400 mt-8">
-        {t("transcription.maximum-capacity")}
-      </p>
-    </div>
-  </div>
+  <div class="relative flex flex-col">
+    <label
+      class={`py-6 relative flex flex-col text-base-content border border-neutral-content border-dashed rounded cursor-pointer ${isDragOver && "border-primary ring-4 ring-inset"}`}
+      on:dragover={() => {
+        isDragOver = true;
+      }}
+      on:dragleave={() => {
+        isDragOver = false;
+      }}
+      on:drop={() => {
+        isDragOver = false;
+      }}
+    >
+      <input
+        type="file"
+        multiple
+        class="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+        on:change={addFiles}
+      />
 
-<div>
-    {fileName }
-</div>
+      <div class="flex flex-col items-center">
+        {@html svgIcons.upload}
+        <p class="text-base font-semibold">
+          {@html t("transcription.input-file-upload-description")}
+        </p>
+        <p class="text-sm text-gray-500 mt-1">
+          {t("transcription.supportted-file-extensions")}
+        </p>
+        <p class="text-xs text-gray-400 mt-8">
+          {t("transcription.maximum-capacity")}
+        </p>
+      </div>
+    </label>
+  </div>
 </div>
