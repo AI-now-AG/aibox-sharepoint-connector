0e9894c1f600ed67dd1ba9c75d39c916d021592a
Devlin <tinhocgocong@gmail.com>
2024-10-21 13:40:35 +0700

    [Fix] Apply lang="ts" for TenantList.svelte

--- src/pages/tenant-management/TenantList.svelte
+++ src/pages/tenant-management/TenantList.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { actions } from "astro:actions";
   import { svgIcons } from "$assets/icons";
   import { useTranslations } from "$i18n/utils";
@@ -10,10 +10,10 @@
 
   const t = useTranslations();
 
-  let tenants = [];
-  let showArchived = false;
-  let searchValue = null;
-  let timeout;
+  let tenants: any[] = [];
+  let showArchived: boolean = false;
+  let searchValue: string = "";
+  let timeout: any;
 
   onMount(async () => {
     await fetchTenants();
