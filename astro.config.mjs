8db94cf81b99094880971e783b06be7809543506
Devlin <tinhocgocong@gmail.com>
2024-10-21 18:37:45 +0700

    [REFACTOR] Sentry Integration for errors tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,23 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [tailwind(), svelte()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    sentry({
+      dsn: "https://3dd6d1e46e7ea1249f102712d6b7742b@o4508102990757888.ingest.de.sentry.io/4508160313983056",
+      sourceMapsUploadOptions: {
+        project: "aibox",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   security: {
     checkOrigin: true,
   },
