81ef694cec809b280f1ef355952b71cb01b5f17f
Steve Nguyen <steve.nguyen@business4you.ch>
2024-10-21 09:50:28 +0700

    feat: AINOW-494 [FE] Implement UI for transcription  - update uploading icon

--- src/pages/transcription/TranscriptionForm.svelte
+++ src/pages/transcription/TranscriptionForm.svelte
@@ -366,7 +366,8 @@
           <div class="flex items-center space-x-4">
             <div class="flex items-center space-x-2">
               {#if !isUploaded}
-                {@html svgIcons.uploading}
+                <span class="loading loading-spinner loading-md text-primary"
+                ></span>
                 <p class="font-medium">{t("transciption.uploading")}</p>
               {/if}
               {#if isTranscribing}
