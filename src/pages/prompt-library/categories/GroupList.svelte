acbd2ecefc8557ae9ab3ef20160dc928a7513337
Devlin <tinhocgocong@gmail.com>
2024-12-20 21:51:35 +0700

    fix: Mobile bugs fix & optimize

--- src/pages/prompt-library/categories/GroupList.svelte
+++ src/pages/prompt-library/categories/GroupList.svelte
@@ -69,15 +69,15 @@
   </div>
 
   <!-- drag and drop list -->
-  <div class="flex items-center bg-base-300 py-3 px-4 rounded-lg">
-    <div class="flex-1 w-auto text-left font-normal text-xs rounded-l-lg">
+  <div class="flex items-center bg-base-300 py-3 px-4 space-x-3 rounded-lg">
+    <div class="flex-1 w-48 md:w-auto text-left font-normal text-xs  space-x-2 rounded-l-lg">
       <span class="inline-flex w-10"></span>
       {t("prompt-library.categories.list.name")}
     </div>
     <div class="flex-none w-20 text-left font-normal text-xs">
       {t("prompt-library.categories.list.status")}
     </div>
-    <div class="flex-none w-20 rounded-r-lg"></div>
+    <div class="flex-none w-10 rounded-r-lg"></div>
   </div>
 
   <section
@@ -87,10 +87,10 @@
   >
     {#each items as item, i (item.id)}
       <div
-        class="flex items-center h-16 bg-base-100 hover:bg-base-300 text-sm rounded-lg py-3 px-4 mt-3 dnd-item"
+        class="flex items-center h-16 bg-base-100 hover:bg-base-300 text-sm rounded-lg py-3 px-4 space-x-2 mt-3 dnd-item"
       >
         <div
-          class="flex-1 w-auto flex items-center text-sm font-medium rounded-l-lg"
+          class="flex-1 w-48 md:w-auto flex items-center text-sm font-medium space-x-3 rounded-l-lg"
         >
           <span class="inline-flex w-10">
             {@html svgIcons.drag}
@@ -111,7 +111,7 @@
           >
         </div>
         <div
-          class="flex-none w-20 text-right relative relative-dropdown rounded-r-lg"
+          class="flex-none w-10 text-right relative relative-dropdown rounded-r-lg"
         >
           <div class="dropdown dropdown-hover dropdown-end">
             <div tabindex="0" role="button" class="btn btn-ghost btn-sm">
