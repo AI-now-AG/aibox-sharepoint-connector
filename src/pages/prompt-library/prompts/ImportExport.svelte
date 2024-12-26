878d042ec4671bb6e7689f609e90acc69e2e701a
Devlin <tinhocgocong@gmail.com>
2024-12-26 14:05:51 +0700

    feat: Finish import upload dialog & import/export buttons

---
+++ src/pages/prompt-library/prompts/ImportExport.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { useTranslations } from "$i18n/utils";
+  import { svgIcons } from "$assets/icons";
+  import ImportUploadDialog from "./ImportUploadDialog.svelte";
+
+  const t = useTranslations();
+
+  let fileUploadModal: HTMLDialogElement;
+  let inputFile: File;
+
+  function handleImport() {
+    console.log("file", inputFile);
+  }
+</script>
+
+<div class="space-x-2">
+  <button
+    class="btn btn-default btn-outline font-normal grow-0"
+    on:click|preventDefault={() => {
+      fileUploadModal.showModal();
+    }}
+  >
+    {@html svgIcons.fileImport}
+    {t("prompt-library.prompts.import")}
+  </button>
+  <a
+    href="/prompts/export"
+    class="btn btn-default btn-outline font-normal grow-0"
+    data-astro-prefetch="false"
+  >
+    {@html svgIcons.fileExport}
+    {t("prompt-library.prompts.export")}
+  </a>
+</div>
+
+<ImportUploadDialog
+  bind:modal={fileUploadModal}
+  bind:file={inputFile}
+  on:upload={handleImport}
+/>
---
+++ src/pages/prompt-library/prompts/ImportUploadDialog.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+  import { useTranslations } from "$i18n/utils";
+  import { createEventDispatcher } from "svelte";
+  import { svgIcons } from "$assets/icons";
+
+  export let modal;
+  export let file: File | undefined;
+
+  const dispatch = createEventDispatcher();
+  const t = useTranslations();
+
+  const acceptedTypes: Record<string, string[]> = {
+    "text/csv": ["text/csv"],
+  };
+  let isDragOver = false;
+  let fileErrorMessage: string = "";
+
+  const acceptedMimeTypes = Object.values(acceptedTypes).flat().join(", ");
+  const units = ["B", "kB", "MB", "GB", "TB"];
+
+  $: {
+    if (file && !isFileValid(file)) {
+      fileErrorMessage = "unsupported-type";
+    } else {
+      fileErrorMessage = "";
+    }
+  }
+
+  function addFiles(
+    event: Event & { currentTarget: EventTarget & HTMLInputElement },
+  ) {
+    const eventTarget = event.target as HTMLInputElement;
+    file = eventTarget?.files?.[0];
+
+    if (!file || !isFileValid(file)) {
+      return;
+    }
+
+    dispatch("upload");
+  }
+
+  function isFileTypeValid(checkType: string) {
+    for (const type in acceptedTypes) {
+      if (acceptedTypes[type].includes(checkType)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function isFileValid(file: File) {
+    const type = file.type;
+    if (isFileTypeValid(type)) {
+      return true;
+    }
+    return false;
+  }
+</script>
+
+<dialog bind:this={modal} class="modal">
+  <div class="modal-box px-4 py-5 rounded-lg mx-auto max-w-3xl">
+    <h3 class="text-lg font-bold pb-4">Upload Files</h3>
+    <form method="dialog">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        >✕</button
+      >
+    </form>
+    <div class="relative flex flex-col py-4">
+      <label
+        class={`py-16 relative flex flex-col text-base-content border border-dashed rounded cursor-pointer ${isDragOver ? "border-blue-500" : "border-neutral-content"} ${fileErrorMessage && "border-red-500 bg-red-100"}`}
+        on:dragover={() => {
+          isDragOver = true;
+        }}
+        on:dragleave={() => {
+          isDragOver = false;
+        }}
+        on:drop={() => {
+          isDragOver = false;
+        }}
+      >
+        <input
+          type="file"
+          accept={acceptedMimeTypes}
+          class="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+          on:change={addFiles}
+        />
+
+        <div class="flex flex-col items-center">
+          {@html svgIcons.upload}
+          <p class="text-base font-semibold">
+            {@html t("prompt-execution.upload-file.drag")}
+          </p>
+          <p class="text-sm text-gray-400 mt-1">CSV</p>
+        </div>
+      </label>
+      <span class="mt-2 text-xs text-red-500">{fileErrorMessage}</span>
+    </div>
+
+    <div class="modal-action">
+      <form method="dialog">
+        <button class="btn">Close</button>
+        <button class="btn btn-primary" type="submit">Upload</button>
+      </form>
+    </div>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
