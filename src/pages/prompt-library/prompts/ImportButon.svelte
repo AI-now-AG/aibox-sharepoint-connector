5430019261d59dd7b71fa8f9e013a214fb300f58
Devlin <tinhocgocong@gmail.com>
2024-12-26 13:10:34 +0700

    feat: Add more svelte components

---
+++ src/pages/prompt-library/prompts/ImportButon.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { useTranslations } from "$i18n/utils";
+  import { svgIcons } from "$assets/icons";
+  import ImportFileUpload from "./ImportFileUpload.svelte";
+
+  const t = useTranslations();
+
+  let fileUploadModal: HTMLDialogElement;
+  let inputFiles: File[] = [];
+</script>
+
+<div>
+  <button
+    class="btn btn-default btn-outline font-normal grow-0"
+    on:click|preventDefault={() => {
+      console.log('aaaa');
+      fileUploadModal.showModal();
+    }}
+  >
+    {@html svgIcons.fileImport}
+    import
+  </button>
+
+  <ImportFileUpload
+    bind:modal={fileUploadModal}
+    bind:files={inputFiles}
+  />
+</div>
