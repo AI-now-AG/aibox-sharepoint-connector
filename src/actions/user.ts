287e034be73d8a5b8a30d37b589cd30b9a410c19
Steve Nguyen <hoanghcmus@gmail.com>
2024-12-30 19:11:56 +0700

    fix: AINOW-688 eslint error

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -66,7 +66,7 @@ export const user = {
         name: input.name,
         email: input.email,
         roles: input.roles,
-        permissions: assignPermissions[input.roles],
+        permissions: assignPermissions(input.roles),
         tenant_id: input.tenant_id,
 
         email_verified: false,
@@ -85,7 +85,7 @@ export const user = {
     input: z.intersection(UserInputParamsSchema, UserInputIdentifierSchema),
     handler: async (input) => {
       // TODO: Update user on Auth0
-      const updatedDocument = await userModel.update(input._id, input);
+      const updatedDocument = await userModel.update(input._id, {});
 
       return transformRawData(updatedDocument);
     },
