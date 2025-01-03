fd801c195d25ea5aff990e74ead3891412d89c8e
Devlin <tinhocgocong@gmail.com>
2025-01-03 20:00:00 +0700

    feat: Test netlify functions

--- src/data/auth0/management-client.ts
+++ src/data/auth0/management-client.ts
@@ -1,8 +1,11 @@
 import { ManagementClient } from "auth0";
 
-const tenant = import.meta.env.AUTH0_TENANT || "ainow";
-const clientId = import.meta.env.AUTH0_MNGT_CLIENT_ID;
-const clientSecret = import.meta.env.AUTH0_MNGT_CLIENT_SECRET;
+const tenant = import.meta?.env?.AUTH0_TENANT || process.env.AUTH0_TENANT;
+const clientId =
+  import.meta?.env?.AUTH0_MNGT_CLIENT_ID || process.env.AUTH0_MNGT_CLIENT_ID;
+const clientSecret =
+  import.meta?.env?.AUTH0_MNGT_CLIENT_SECRET ||
+  process.env.AUTH0_MNGT_CLIENT_SECRET;
 
 export const management = new ManagementClient({
   domain: `${tenant}.eu.auth0.com`,
