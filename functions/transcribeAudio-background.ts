b24b167605d9464bebe6492f2df5fdf38894ef1b
Vishal <vishal@ainow.ch>
2025-01-16 22:08:32 +0530

    update dir

--- functions/transcribeAudio-background.ts
+++ functions/transcribeAudio-background.ts
@@ -268,7 +268,7 @@ async function processWithFFmpeg(
   format: string,
 ): Promise<void> {
   const newFormat = format === "m4a" ? "mp3" : format;
-  const tempDir = path.join(__dirname, "temp");
+  const tempDir = path.join("/tmp", "temp");
   if (!existsSync(tempDir)) {
     mkdirSync(tempDir, { recursive: true });
     console.log("Created directory:", tempDir);
