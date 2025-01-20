9a00c648c550d4f1d6f61fee87e80e44ed908dba
Vishal <vishal@ainow.ch>
2025-01-20 23:54:17 +0530

    cleanup

--- functions/postAudioProProcess-background.ts
+++ functions/postAudioProProcess-background.ts
@@ -16,7 +16,7 @@ const postAudioProProcess: Handler = async (
       body: JSON.stringify({ message: "Method Not Allowed" }),
     };
   }
-
+  console.log("postAudioProProcess called");
   try {
     const body = JSON.parse(event.body || "{}") as {
       uniqueName: string;
@@ -25,7 +25,7 @@ const postAudioProProcess: Handler = async (
       jsonData: TranscriptionResponse;
       transcriptionText: string;
     };
-
+    console.log("postAudioProProcess body", body);
     const { uniqueName, uploadUrl, folderName, jsonData, transcriptionText } =
       body;
 
@@ -35,21 +35,21 @@ const postAudioProProcess: Handler = async (
         body: JSON.stringify({ message: "Invalid file upload data" }),
       };
     }
-
+    console.log("postAudioProProcess uniqueName", uniqueName);
     const outputURLs = await uploadLargeFile(
       uploadUrl,
       folderName,
       jsonData,
       transcriptionText,
     );
-
+    console.log("postAudioProProcess outputURLs", outputURLs);
     await updateTask(uniqueName, {
       status: "completed",
       txtUrl: outputURLs.txt,
       srtUrl: outputURLs.srt || "",
       assUrl: outputURLs.ass || "",
     });
-
+    console.log("data-----");
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -58,6 +58,7 @@ const postAudioProProcess: Handler = async (
       }),
     };
   } catch (error) {
+    console.log("Error------:", error);
     console.error("Error while processing post-audio:", error);
 
     const body = JSON.parse(event.body || "{}") as { uniqueName?: string };
@@ -67,12 +68,12 @@ const postAudioProProcess: Handler = async (
       error instanceof Error
         ? error.message
         : "Unknown error during post-audio process.";
-
+    console.log("error Message::::", errorMessage);
     await updateTask(uniqueName, {
       status: "failed",
       error: errorMessage,
     });
-
+    console.log("error:------");
     return {
       statusCode: 500,
       body: JSON.stringify({ message: errorMessage }),
