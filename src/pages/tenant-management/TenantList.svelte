e5fb2217e0b2754a8f77bb81d778c6c87a6ad49a
Devlin <tinhocgocong@gmail.com>
2025-01-24 16:08:06 +0700

    Remove default empty

--- src/pages/tenant-management/TenantList.svelte
+++ src/pages/tenant-management/TenantList.svelte
@@ -93,7 +93,7 @@
   }
 
   async function deleteTenant() {
-    const { _id = "" } = selectedTenant;
+    const { _id } = selectedTenant;
     let result = await actions.tenant.delete({
       _id,
     });
