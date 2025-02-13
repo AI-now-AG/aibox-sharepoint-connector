cd4d0b50a052bfd8061d8f5572c06686b9df265b
Steve Nguyen <steve.nguyen@business4you.ch>
2025-02-13 11:34:08 +0700

    feat: AINOW-784 Create new Tenant: setup account for admin - [AINOW-803] Implement UI for Add Tenant Dialog

---
+++ src/components/InputDialog.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { useTranslations } from "$i18n/utils";
+
+  const t = useTranslations();
+  interface Props {
+    modal: any;
+    title?: string;
+    label?: string;
+    placeholder?: string;
+    value?: string;
+    save?: Function;
+  }
+
+  let {
+    modal = $bindable(),
+    title = "",
+    label = "",
+    placeholder = label,
+    value = $bindable(""),
+    save = () => null,
+  }: Props = $props();
+
+  function clear() {
+    value = "";
+  }
+  
+</script>
+
+<dialog
+  id="alert_dialog"
+  bind:this={modal}
+  class="modal"
+  style="background-color: rgba(0,0,0,0.5);"
+>
+  <div class="modal-box">
+    <form method="dialog">
+      <span class=" absolute left-6 top-4 mb-2">{title}</span>
+      <button
+        class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        onclick={() => {
+          clear();
+        }}>✕</button
+      >
+    </form>
+
+    <div class="flex-1 flex flex-col mt-8">
+      <span class="mb-2 text-gray-400 font-medium text-sm">{label}</span>
+      <input
+        type="text"
+        {placeholder}
+        class="input input-bordered w-full"
+        bind:value
+      />
+    </div>
+
+    <div class="flex space-x-2 ml-auto mt-4 justify-end">
+      <button
+        class="btn btn-primary"
+        onclick={() => {
+          save?.(value);
+          modal.close();
+        }}
+      >
+        {t("common.save")}
+      </button>
+      <button
+        class="btn"
+        onclick={() => {
+          clear();
+          modal.close();
+        }}
+      >
+        {t("common.cancel")}
+      </button>
+    </div>
+  </div>
+</dialog>
