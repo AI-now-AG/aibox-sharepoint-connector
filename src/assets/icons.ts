2a9f15c3605717de2dbb3cfc02f5d797d2efac00
Steve Nguyen <steve.nguyen@business4you.ch>
2025-02-14 15:27:27 +0700

    chore: [AINOW-671] Prompt execution improvements - clean up svg icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -44,6 +44,8 @@ import arrowDownFill from "$assets/icons/arrow-down-fill.svg?raw";
 import arrowUp from "$assets/icons/arrow-up.svg?raw";
 import arrowDown from "$assets/icons/arrow-down.svg?raw";
 import arrowUpDown from "$assets/icons/arrow-up-down.svg?raw";
+import sendActive from "$assets/icons/send-active.svg?raw";
+import sendInActive from "$assets/icons/send-inactive.svg?raw";
 
 export const svgIcons = {
   menuSettings,
@@ -92,4 +94,6 @@ export const svgIcons = {
   arrowUp,
   arrowDown,
   arrowUpDown,
+  sendActive,
+  sendInActive,
 };
