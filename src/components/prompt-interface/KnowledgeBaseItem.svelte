6676a9557ccdb9e9461764d87663202c16e24f8f
Steve Nguyen <steve.nguyen@business4you.ch>
2025-02-27 14:25:15 +0700

    feat: AINOW-847 [FE] [List screen] Implement UI for Knowledge Base item (three dot menu options, remove View CTA and Delete icon)

---
+++ src/components/prompt-interface/KnowledgeBaseItem.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import DropdownSection from "$components/DropdownSection.svelte";
+  import { type Option } from "$components/DropdownOptions.svelte";
+  import { fade } from "svelte/transition";
+  import { svgIcons } from "$assets/icons";
+  import { useTranslations } from "$i18n/utils";
+
+  const t = useTranslations();
+
+  interface Props {
+    isEditable: boolean;
+    data: any;
+    onSelectEdit?: Function;
+    onSelectDuplicate?: Function;
+    onSelectDelete?: Function;
+  }
+
+  let {
+    isEditable,
+    data,
+    onSelectEdit,
+    onSelectDuplicate,
+    onSelectDelete,
+  }: Props = $props();
+
+  let options: Option[] = [
+    {
+      icon: svgIcons.edit,
+      text: t("common.edit"),
+      action: () => {
+        onSelectEdit?.();
+      },
+    },
+    {
+      icon: svgIcons.duplicate,
+      text: t("common.duplicate"),
+      action: () => {
+        onSelectDuplicate?.();
+      },
+    },
+    {
+      icon: svgIcons.trash,
+      text: t("common.delete"),
+      action: () => {
+        onSelectDelete?.();
+      },
+    },
+  ];
+</script>
+
+<div class="card bg-base-100 shadow-xl" out:fade>
+  <div class="card-body space-y-2 justify-between">
+    {#if data?.tags}
+      <div class="card-actions justify-start">
+        {#each data?.tags as tag}
+          <div class="badge px-2 border-base-300">
+            {tag}
+          </div>
+        {/each}
+      </div>
+    {/if}
+    <h2 class="card-title">{data?.title}</h2>
+    {#if data?.description}
+      <p class="text-base-content/60 line-clamp-3">
+        {data?.description}
+      </p>
+    {/if}
+  </div>
+  {#if isEditable}
+    <DropdownSection class={"absolute top-6 right-3"} {options} />
+  {/if}
+</div>
---
+++ src/components/prompt-interface/KnowledgeBaseList.svelte
@@ -0,0 +1,125 @@
+<script lang="ts" module>
+  export interface KnowledgeBaseCardItem {
+    id: string;
+    title: string;
+    description?: string;
+    instruction?: string;
+  }
+</script>
+
+<script lang="ts">
+  import KnowledgeBaseItem from "./KnowledgeBaseItem.svelte";
+  import ConfirmDialog from "$components/ConfirmDialog.svelte";
+  import Loading from "$components/Loading.svelte";
+  import { loading, showLoading, hideLoading } from "$stores";
+  import { addToast } from "$stores/toast";
+  import { useTranslations } from "$i18n/utils";
+
+  const t = useTranslations();
+
+  interface Props {
+    items?: KnowledgeBaseCardItem[];
+    title?: string;
+    isEditable?: boolean;
+  }
+
+  let {
+    items = $bindable([]),
+    title = t("prompt-library.knowledgebase.all"),
+    isEditable = false,
+  }: Props = $props();
+
+  $inspect(items);
+
+  let selectedEditKnowledgeBaseId: string = $state("");
+  let selectedDeletePKnowledgeBaseId: string = "";
+
+  let confirmDeleteModal: HTMLDialogElement | undefined = $state();
+
+  async function editCard(index: number) {
+    selectedEditKnowledgeBaseId = items[index]?.id ?? "";
+  }
+
+  async function duplicateCard(index: number) {
+    selectedEditKnowledgeBaseId = items[index]?.id ?? "";
+  }
+
+  function onDeleteCard(index: number) {
+    selectedDeletePKnowledgeBaseId = items[index]?.id ?? "";
+    confirmDeleteModal?.show();
+  }
+
+  function removeDeletedItem(deletedId: string) {
+    items = items?.filter((item: any) => item.id !== deletedId);
+  }
+
+  async function deleteCard() {
+    try {
+      showLoading();
+      const deletedKnowledgeBase = {
+        ...(selectedDeletePKnowledgeBaseId && {
+          _id: selectedDeletePKnowledgeBaseId,
+        }),
+      };
+      const response = await fetch("/api/knowledge-base.json", {
+        method: "DELETE",
+        body: JSON.stringify(deletedKnowledgeBase),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          errorData.message || t("prompt-library.delete.knowledge-base.failed"),
+        );
+      }
+      removeDeletedItem(selectedDeletePKnowledgeBaseId);
+      hideLoading();
+      addToast({
+        message: t("prompt-library.delete.knowledge-base.success"),
+        type: "success",
+      });
+    } catch (error) {
+      hideLoading();
+      addToast({
+        message:
+          error instanceof Error ? error.message : t("common.unexpected.error"),
+        type: "error",
+      });
+    }
+  }
+</script>
+
+<div class="container max-w-5xl mx-auto p-6 space-y-4">
+  <h1 class="text-lg font-normal text-base-content/80">
+    {title}
+  </h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+    {#each items as card, index}
+      <KnowledgeBaseItem
+        {isEditable}
+        data={card}
+        onSelectEdit={() => {
+          editCard(index);
+        }}
+        onSelectDuplicate={() => {
+          duplicateCard(index);
+        }}
+        onSelectDelete={() => {
+          onDeleteCard(index);
+        }}
+      />
+    {/each}
+  </div>
+</div>
+
+<ConfirmDialog
+  bind:modal={confirmDeleteModal}
+  confirm={deleteCard}
+  title={t("prompt-library.delete.knowledge-base.confirm")}
+/>
+
+<Loading bind:show={$loading} />
