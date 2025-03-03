1e9578af57a8c72ae41b2dee58353cdbbbc7eb5f
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-03 13:08:58 +0700

    feat: AINOW-853 [FE] [List screen] Handle refresh list knowledgebase using the store

--- src/pages/prompt-library/knowledge-base/KnowledgeBaseForm.svelte
+++ src/pages/prompt-library/knowledge-base/KnowledgeBaseForm.svelte
@@ -6,7 +6,7 @@
   import { svgIcons } from "$assets/icons";
   import { preventDefault } from "$utils/common";
   import TextEditor from "$components/TextEditor.svelte";
-  import { refreshTrigger } from '$stores';
+  import { refreshTrigger } from "$stores";
 
   const t = useTranslations();
 
@@ -77,7 +77,7 @@
 
       const data = await response.json();
       window.history.back();
-      refreshTrigger.update(n => n + 1);
+      refreshTrigger.update((n) => n + 1);
 
       addToast({
         message: data.message,
