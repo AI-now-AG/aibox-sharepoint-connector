05c1e0747aa4a28dea167f7a5bb85e2fb06f1837
Devlin <tinhocgocong@gmail.com>
2025-03-05 18:55:57 +0700

    fix: Update webhook functions

--- src/utils/auth0Auth.ts
+++ src/utils/auth0Auth.ts
@@ -36,7 +36,6 @@ export const sendPasswordResetEmail = async (
     console.log(`Password reset email sent to ${email}`);
   } catch (error) {
     console.error("Failed to send password reset email:", error);
-    throw new Error("Failed to send password reset email.");
   }
 };
 
@@ -92,6 +91,5 @@ export const sendVerificationEmail = async (userId: string) => {
     console.log(`Verification email sent successfully: ${ticket}`);
   } catch (error) {
     console.error("Error sending verification email:", error);
-    throw new Error("Failed to send verification email.");
   }
 };
