b478f93cdeb5c73a63d3a1867f1a9638477302cd
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-07 14:49:52 +0700

    feat: AINOW-858 [FE] Evaluate onboarding libraries - try intro.js - update demo

--- src/components/OnboardingTour.svelte
+++ src/components/OnboardingTour.svelte
@@ -8,19 +8,23 @@
       .setOptions({
         steps: [
           {
-            intro: "Welcome to AIBOX APP! Let's take a tour.",
+            intro: "We want to help you get the best experience out of your trial of aibox.\n\n Let’s go on a tour.",
           },
           {
             element: "#onboardingId1",
-            intro: "Select a prompt menu item",
+            intro:
+              "Let AI help you with your processes. We already provided some examples for you.\n\nIn the paid version, you can define the structure and the use cases yourself, add knowledge bases and invite your team!",
+            position: "right",
           },
           {
             element: "#onboardingId2",
-            intro: "Enter your chat",
+            intro: "You don’t need to provide all the information you need.",
+            position: "left",
           },
-          {
+          { 
             element: "#onboardingId3",
             intro: "Setup your profile",
+            position: "right",
           },
           {
             intro: "Enjoy using AIBOX.",
