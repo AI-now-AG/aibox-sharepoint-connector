c06ec9ec316c335b88a0d5ea17d4e46c92ea7e02
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-07 13:24:12 +0700

    feat: AINOW-858 [FE] Evaluate onboarding libraries - try intro.js - create demo

---
+++ src/components/OnboardingTour.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { onMount } from "svelte";
+  import introJs from "intro.js";
+  import "intro.js/introjs.css";
+
+  onMount(() => {
+    introJs()
+      .setOptions({
+        steps: [
+          {
+            intro: "Welcome to AIBOX APP! Let's take a tour.",
+          },
+          {
+            element: "#onboardingId1",
+            intro: "Select a prompt menu item",
+          },
+          {
+            element: "#onboardingId2",
+            intro: "Enter your chat",
+          },
+          {
+            element: "#onboardingId3",
+            intro: "Setup your profile",
+          },
+          {
+            intro: "Enjoy using AIBOX.",
+          },
+        ],
+      })
+      .start();
+  });
+</script>
+
+<div>
+  <!-- Your onboarding content which will be highlighted -->
+</div>
+
+<style>
+  /* Custom styles if needed */
+</style>
