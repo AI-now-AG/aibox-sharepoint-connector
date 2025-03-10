14545165c10ad6139752c83eccbebe924abe9c6a
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-10 17:15:33 +0700

    feat: AINOW-862 [FE] Implement Onboarding flow follow design - add text translation and clean up

--- src/components/OnboardingTour.svelte
+++ src/components/OnboardingTour.svelte
@@ -3,16 +3,18 @@
   import { driver } from "driver.js";
   import "driver.js/dist/driver.css";
 
+  import { useTranslations } from "$i18n/utils";
+  const t = useTranslations();
+
+  let _popover;
+
   onMount(() => {
-    let _popover;
     const driverObj = driver({
-      showProgress: false,
-      smoothScroll: true,
       popoverClass: "driverjs-theme",
       overlayClickBehavior: "nextStep",
       showButtons: ["next", "previous"],
-      nextBtnText: "Next →",
-      prevBtnText: "Skip",
+      prevBtnText: t("onboarding.skip"),
+      nextBtnText: t("onboarding.next"),
       onPrevClick: () => {
         driverObj.destroy();
       },
@@ -20,38 +22,35 @@
         {
           element: "#onboardingId0",
           popover: {
-            title: "Welcome to aibox",
-            description:
-              "We want to help you get the best experience out of your trial of aibox. <br/><br/> Let’s go on a tour.",
+            showButtons: ["next"],
             popoverClass: "driverjs-theme",
+            title: t("onboarding.step-0.title"),
+            description: t("onboarding.step-0.description"),
+            nextBtnText: t("onboarding.step-0.next-btn-text"),
           },
         },
         {
           element: "#onboardingId1",
           popover: {
-            title: "Execute your use cases",
-            description:
-              "Let AI help you with your processes. We already provided some examples for you.<br/><br/>In the paid version, you can define the structure and the use cases yourself, add knowledge bases and invite your team!",
-            side: "right",
             popoverClass: "driverjs-theme",
+            title: t("onboarding.step-1.title"),
+            description: t("onboarding.step-1.description"),
           },
         },
         {
           element: "#onboardingId2",
           popover: {
-            title: "Execute your use cases",
-            description:
-              "You don’t need to provide all the information you need. ",
             popoverClass: "driverjs-theme",
+            title: t("onboarding.step-2.title"),
+            description: t("onboarding.step-2.description"),
           },
         },
         {
           element: "#onboardingId3",
           popover: {
-            title: "Transcribe your audio",
-            description: "Transcribe your audio",
-            side: "right",
             popoverClass: "driverjs-theme",
+            title: t("onboarding.step-3.title"),
+            description: t("onboarding.step-3.description"),
           },
         },
       ],
@@ -59,10 +58,14 @@
         _popover = document.getElementById("driver-popover-content");
         _popover.style.animation = "animate-fade-in .3s";
         if (state.activeStep.element == "#onboardingId0") {
-          const disabledPreButton = document.getElementsByClassName(
-            "driver-popover-prev-btn driver-popover-btn-disabled",
-          )[0];
-          disabledPreButton.style.visibility = "hidden";
+          document.getElementsByClassName(
+            "driver-popover-navigation-btns",
+          )[0].style.justifyContent = "center";
+          document.getElementById("driver-popover-title").style.textAlign =
+            "center";
+          document.getElementById(
+            "driver-popover-description",
+          ).style.textAlign = "center";
         }
       },
       onHighlighted: (_e, _step) => {
@@ -85,11 +88,6 @@
 </script>
 
 <style>
-  /* :global(.driver-popover.driverjs-theme) {
-    background-color: #fde047;
-    color: #000;
-  } */
-
   :global(.driver-popover.driverjs-theme .driver-popover-title) {
     color: #491eff;
     margin-bottom: 2vh;
@@ -112,9 +110,6 @@
     font-weight: 700;
   }
 
-  /* :global(.driver-popover.driverjs-theme button:hover) {
-  } */
-
   :global(.driver-popover.driverjs-theme .driver-popover-next-btn) {
     background-color: #4338ca;
     color: white;
@@ -128,32 +123,4 @@
   :global(.driver-popover.driverjs-theme .driver-popover-close-btn:hover) {
     color: #000;
   }
-
-  /* :global(
-    .driver-popover.driverjs-theme
-      .driver-popover-arrow-side-left.driver-popover-arrow
-  ) {
-    border-left-color: #fde047; 
-  } */
-
-  /* :global(
-    .driver-popover.driverjs-theme
-      .driver-popover-arrow-side-right.driver-popover-arrow
-  ) {
-    border-right-color: #fde047; 
-  } */
-
-  /* :global(
-    .driver-popover.driverjs-theme
-      .driver-popover-arrow-side-top.driver-popover-arrow
-  ) {
-    border-top-color: #fde047;
-  } */
-
-  /* :global(
-    .driver-popover.driverjs-theme
-      .driver-popover-arrow-side-bottom.driver-popover-arrow
-  ) {
-    border-bottom-color: #fde047;
-  } */
 </style>
