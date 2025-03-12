be65009a7986fca90cfba18b23f4c9c87951111d
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-12 14:49:36 +0700

    feat: AINOW-871 Provide "trial version" flag in tenant management - add trial component

---
+++ src/components/TrialComponent.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { useTranslations } from "$i18n/utils";
+
+  const t = useTranslations();
+
+  interface Props {
+    user: any;
+  }
+  let { user } = $props() as Props;
+
+  onMount(async () => {});
+</script>
+
+<div
+  class="fixed bottom-4 right-4 bg-green-300 text-sm text-gray-800 p-2 pl-4 pr-4 rounded-md shadow-lg"
+>
+  This is a free trial version, <a
+    href="/"
+    class="font-normal text-blue-600 hover:underline">contact us</a
+  > to upgrade.
+</div>
