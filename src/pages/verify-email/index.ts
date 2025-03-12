d0e4c88de278dd0053ad06a9cebedad2b9f8879f
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-12 17:55:44 +0700

    feat: AINOW-875 Show message for Users with Unverified Emails - handle send verification email

--- src/pages/verify-email/index.ts
+++ src/pages/verify-email/index.ts
@@ -1,6 +1,15 @@
+import { sendVerificationEmail } from "$utils/auth0Auth";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext): Promise<Response> {
-  // TODO: Handle sending the verification email
-  return context.redirect("/");
+  try {
+    const userId = context.locals.user.id.toString();
+    const email = context.locals.user.email;
+
+    await sendVerificationEmail(userId, email);
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  } finally {
+    return context.redirect("/");
+  }
 }
