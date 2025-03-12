1c06ab1fd76b77d2fb6c07a6599995c7fa202553
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-12 14:57:06 +0700

    feat: AINOW-871 Provide "trial version" flag in tenant management - add trial component - open link on new tab

--- src/components/TrialComponent.svelte
+++ src/components/TrialComponent.svelte
@@ -18,7 +18,7 @@
     class="fixed bottom-4 right-4 bg-green-300 text-sm text-gray-800 p-2 pl-4 pr-4 rounded-md shadow-lg"
   >
     {t("tenant.this-is-free-trial-version")}
-    <a href="/" class="font-normal text-blue-600 hover:underline">
+    <a href="/" target="_blank" class="font-normal text-blue-600 hover:underline">
       {t("tenant.contact-us")}</a
     >
     {t("tenant.to-upgrade")}.
