efe3d02d4ab046e3ba70677064053dd28e9da2e2
Steve Nguyen <steve.nguyen@business4you.ch>
2025-03-12 14:55:52 +0700

    feat: AINOW-871 Provide "trial version" flag in tenant management - add trial component - check show trial component or not

--- src/components/TrialComponent.svelte
+++ src/components/TrialComponent.svelte
@@ -6,19 +6,21 @@
   const t = useTranslations();
 
   interface Props {
-    user: any;
+    tenant: any;
   }
-  let { user } = $props() as Props;
+  let { tenant } = $props() as Props;
 
   onMount(async () => {});
 </script>
 
-<div
-  class="fixed bottom-4 right-4 bg-green-300 text-sm text-gray-800 p-2 pl-4 pr-4 rounded-md shadow-lg"
->
-  {t("tenant.this-is-free-trial-version")}
-  <a href="/" class="font-normal text-blue-600 hover:underline">
-    {t("tenant.contact-us")}</a
+{#if tenant?.is_trial}
+  <div
+    class="fixed bottom-4 right-4 bg-green-300 text-sm text-gray-800 p-2 pl-4 pr-4 rounded-md shadow-lg"
   >
-  {t("tenant.to-upgrade")}.
-</div>
+    {t("tenant.this-is-free-trial-version")}
+    <a href="/" class="font-normal text-blue-600 hover:underline">
+      {t("tenant.contact-us")}</a
+    >
+    {t("tenant.to-upgrade")}.
+  </div>
+{/if}
