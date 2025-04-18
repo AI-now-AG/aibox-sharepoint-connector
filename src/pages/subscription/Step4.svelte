3e9dbc66ce621090b1c0cc145ffaa5071ec5c07e
Steve Nguyen <hoanghcmus@gmail.com>
2025-04-18 16:40:48 +0700

    feat: AINOW-969 [FE] Handle UI flow (contact --> subscribe --> create organization --> complete --> Home) - add step4 to complete

--- src/pages/subscription/Step4.svelte
+++ src/pages/subscription/Step4.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
   import { useTranslations } from "$i18n/utils";
   const t = useTranslations();
+
+  function handleNext() {
+    // TODO: Handle validate before go to next step
+    window.location.href = "/subscription/complete";
+  }
 </script>
 
-<div class="max-w-5xl mx-auto"></div>
+<div class="max-w-5xl mx-auto">
+  <div class="fixed bottom-4 right-10 w-[33.3%] flex items-center justify-end">
+    <button
+      class="btn btn-active btn-primary min-w-[144px]"
+      onclick={() => {
+        handleNext();
+      }}
+    >
+      {t("common.next")}
+    </button>
+  </div>
+</div>
