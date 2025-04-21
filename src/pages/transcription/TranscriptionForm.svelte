6cfa8f550ca413f4fc44700c0a63dd138880473b
Vishal <>
2025-04-21 14:31:41 +0530

    update file size

--- src/pages/transcription/TranscriptionForm.svelte
+++ src/pages/transcription/TranscriptionForm.svelte
@@ -132,7 +132,7 @@
       //maxFileSize = 25; // this is for testing purpose
     }
 
-    if (category === AudioCategory.SubtitleLarge) {
+    if (category === AudioCategory.SubtitleLarge || category === AudioCategory.Subtitle11Labs) {
       maxFileSize = 50;
     }
 
@@ -982,7 +982,7 @@
             <p class="text-xs text-base-content/40 mt-8">
               {#if category === AudioCategory.AudioPro}
                 {t("transcription.maximum-capacity-1gb")}
-              {:else if category === AudioCategory.SubtitleLarge}
+              {:else if (category === AudioCategory.SubtitleLarge || AudioCategory.Subtitle11Labs)}
                 {t("transcription.maximum-capacity-50mb")}
               {:else}
                 {t("transcription.maximum-capacity-25mb")}
