3ae458e73e747a6bda32c21152ddbf73a2451a21
Steve Nguyen <steve.nguyen@business4you.ch>
2025-04-22 14:10:58 +0700

    feat:  AINOW-962 [FE] Implement UI screen Creating Account and Congratulation - Basic UI for completion screen

---
+++ src/pages/subscription/Complete.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { useTranslations } from "$i18n/utils";
+  const t = useTranslations();
+
+
+</script>
+
+<div
+  class="max-w-5xl mx-auto flex flex-col justify-center items-center min-h-full"
+>
+  <div
+    class="bg-[#00CFBD] w-[80px] h-[80px] rounded-full flex justify-center items-center mb-6"
+  >
+  <span class="text-white text-4xl">
+    ✓
+  </span>
+  </div>
+</div>
--- src/pages/subscription/Step4.svelte
+++ src/pages/subscription/Step4.svelte
@@ -1,25 +1,37 @@
 <script lang="ts">
   import SubsciptionSteps from "$components/subscription/SubsciptionSteps.svelte";
   import { useTranslations } from "$i18n/utils";
+  import { onMount } from "svelte";
   import RadarLoading from "./RadarLoading.svelte";
   const t = useTranslations();
+
+  function handleSubscription() {
+    // TODO: Devlin - integrate API to create subscription
+    // Simulate a delay of 2 seconds
+    setTimeout(() => {
+      // Redirect to the next step after the delay
+      window.location.href = "/subscription/complete";
+    }, 2000);
+  }
+
+  onMount(() => {handleSubscription()});
 </script>
 
 <div
   class="max-w-5xl mx-auto flex flex-col justify-center items-center min-h-full relative"
 >
   <div
-    class="bg-[#491EFF] p-4 rounded-lg mb-6 flex md:hidden lg:hidden items-center justify-center absolute top-0 mx-6 mt-6"
+    class="bg-[#491EFF] p-4 rounded-lg mb-6 flex md:hidden lg:hidden items-center justify-center absolute top-0"
   >
     <SubsciptionSteps currentStep={4} />
   </div>
 
-
-    <div class="w-[40px] h-[40px] absolute left-[50%] top-[42%] translate-[-50%] flex flex-col justify-center mb-6">
-      <RadarLoading />
-    </div>
-    <p class="font-sans text-3xl font-bold text-[#0F172A] text-center">
-      {t("subscription.creating-your-account")}
-    </p>
- 
+  <div
+    class="w-[40px] h-[40px] absolute left-[50%] top-[42%] translate-[-50%] flex flex-col justify-center mb-6"
+  >
+    <RadarLoading />
+  </div>
+  <p class="font-sans text-3xl font-bold text-[#0F172A] text-center">
+    {t("subscription.creating-your-account")}
+  </p>
 </div>
