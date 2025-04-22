f713827cf54f7088dd869e9a5ef48b6d924dad8f
Steve Nguyen <steve.nguyen@business4you.ch>
2025-04-22 13:43:39 +0700

    feat:  AINOW-962 [FE] Implement UI screen Creating Account and Congratulation - add loading animation

---
+++ src/pages/subscription/RadarLoading.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+</script>
+
+<div class="w-[40px] h-[40px] relative">
+  <div class="circle"></div>
+  <div class="circle2"></div>
+  <div class="circle3"></div>
+  <div class="circle4"></div>
+</div>
+
+<style>
+  .circle {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #2196f3;
+    position: absolute;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    -webkit-animation-timing-function: linear; /* Chrome, Safari, Opera */
+    animation: scaler 4s infinite;
+    animation-timing-function: linear;
+  }
+
+  .circle2 {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #2196f3;
+    position: absolute;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    -webkit-animation-timing-function: linear; /* Chrome, Safari, Opera */
+    animation: scaler 4s infinite;
+    animation-timing-function: linear;
+    -webkit-animation-delay: 1s;
+    animation-delay: 1s;
+  }
+
+  .circle3 {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #2196f3;
+    position: absolute;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    -webkit-animation-timing-function: linear; /* Chrome, Safari, Opera */
+    animation: scaler 4s infinite;
+    animation-timing-function: linear;
+    -webkit-animation-delay: 2s;
+    animation-delay: 2s;
+  }
+
+  .circle4 {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #2196f3;
+    position: absolute;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    -moz-animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    animation: scaler 4s infinite; /* Chrome, Safari, Opera */
+    -webkit-animation-timing-function: linear; /* Chrome, Safari, Opera */
+    animation: scaler 4s infinite;
+    animation-timing-function: linear;
+    -webkit-animation-delay: 3s;
+    animation-delay: 3s;
+  }
+
+  @-webkit-keyframes scaler {
+    0% {
+      -webkit-transform: scale(1);
+    }
+
+    33% {
+      -webkit-transform: scale(2);
+      opacity: 0.2;
+    }
+
+    67% {
+      -webkit-transform: scale(3);
+      opacity: 0.1;
+    }
+
+    100% {
+      -webkit-transform: scale(4);
+      opacity: 0;
+    }
+  }
+
+  @keyframes scaler {
+    0% {
+      transform: scale(1);
+    }
+
+    33% {
+      transform: scale(2);
+      opacity: 0.2;
+    }
+
+    67% {
+      transform: scale(3);
+      opacity: 0.1;
+    }
+
+    100% {
+      transform: scale(4);
+      opacity: 0;
+    }
+  }
+</style>
--- src/pages/subscription/Step4.svelte
+++ src/pages/subscription/Step4.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
   import SubsciptionSteps from "$components/subscription/SubsciptionSteps.svelte";
   import { useTranslations } from "$i18n/utils";
+  import RadarLoading from "./RadarLoading.svelte";
   const t = useTranslations();
-
-  function handleNext() {
-    // TODO: Handle validate before go to next step
-    window.location.href = "/subscription/complete";
-  }
 </script>
 
-<div class="max-w-5xl mx-auto">
+<div
+  class="max-w-5xl mx-auto flex flex-col justify-center items-center min-h-full relative"
+>
   <div
-    class="bg-[#491EFF] p-4 rounded-lg mb-6 flex md:hidden lg:hidden items-center justify-center"
+    class="bg-[#491EFF] p-4 rounded-lg mb-6 flex md:hidden lg:hidden items-center justify-center absolute top-0 mx-6 mt-6"
   >
     <SubsciptionSteps currentStep={4} />
   </div>
-  <div class="w-full flex items-center justify-end rounded-lg p-4">
-    <button
-      class="btn btn-active btn-primary min-w-[144px]"
-      onclick={() => {
-        handleNext();
-      }}
-    >
-      {t("common.next")}
-    </button>
-  </div>
+
+
+    <div class="w-[40px] h-[40px] absolute left-[50%] top-[42%] translate-[-50%] flex flex-col justify-center mb-6">
+      <RadarLoading />
+    </div>
+    <p class="font-sans text-3xl font-bold text-[#0F172A] text-center">
+      {t("subscription.creating-your-account")}
+    </p>
+ 
 </div>
