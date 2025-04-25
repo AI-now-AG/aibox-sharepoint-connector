f86a235d22768f1471bab24125b3afdf4ab4c0bd
Steve Nguyen <hoanghcmus@gmail.com>
2025-04-25 13:41:37 +0700

    feat: AINOW-981 - Self onboarding Improvements - Allow click on Previous step to go back

--- src/components/subscription/SubsciptionSteps.svelte
+++ src/components/subscription/SubsciptionSteps.svelte
@@ -1,3 +1,6 @@
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<!-- svelte-ignore event_directive_deprecated -->
 <script lang="ts">
   import { SUBSCIPTION_STEP } from "$constants";
   import { useTranslations } from "$i18n/utils";
@@ -21,85 +24,151 @@
   const step2Text = t("subscription.step2");
   const step3Text = t("subscription.step3");
   const step4Text = t("subscription.step4");
+
+  const handleClickOnStep = (step: number) => {
+    if (
+      currentStep <= SUBSCIPTION_STEP.Step0 ||
+      currentStep >= SUBSCIPTION_STEP.Step4 ||
+      currentStep <= step
+    ) {
+      return;
+    }
+
+    if (step > SUBSCIPTION_STEP.Step0 && step < SUBSCIPTION_STEP.Step4) {
+      window.location.href = `/subscription/step${step}`;
+    }
+  };
 </script>
 
 <ul class={cssClasses}>
   {#if currentStep === SUBSCIPTION_STEP.Step0}
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}>
       {step1Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}>
       {step2Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}>
       {step3Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}>
       {step4Text}
     </li>
   {:else if currentStep === SUBSCIPTION_STEP.Step1}
-    <li class="step step-primary text-white">
+    <li
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}
+    >
       {step1Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}>
       {step2Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}>
       {step3Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}>
       {step4Text}
     </li>
   {:else if currentStep === SUBSCIPTION_STEP.Step2}
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}
+    >
       {step1Text}
     </li>
-    <li class="step step-primary text-white">
+    <li
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}
+    >
       {step2Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}>
       {step3Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}>
       {step4Text}
     </li>
   {:else if currentStep === SUBSCIPTION_STEP.Step3}
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}
+    >
       {step1Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}
+    >
       {step2Text}
     </li>
-    <li class="step step-primary text-white">
+    <li
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}
+    >
       {step3Text}
     </li>
-    <li class="step">
+    <li class="step" on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}>
       {step4Text}
     </li>
   {:else if currentStep === SUBSCIPTION_STEP.Step4}
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}
+    >
       {step1Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}
+    >
       {step2Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}
+    >
       {step3Text}
     </li>
-    <li class="step step-primary text-white">
+    <li
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}
+    >
       {step4Text}
     </li>
   {:else if currentStep === SUBSCIPTION_STEP.Completed}
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step1)}
+    >
       {step1Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step2)}
+    >
       {step2Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step3)}
+    >
       {step3Text}
     </li>
-    <li data-content="✓" class="step step-primary text-white">
+    <li
+      data-content="✓"
+      class="step step-primary text-white"
+      on:click={() => handleClickOnStep(SUBSCIPTION_STEP.Step4)}
+    >
       {step4Text}
     </li>
   {/if}
@@ -111,6 +180,7 @@
     margin: 0 !important;
     border: none !important;
     background-color: transparent !important;
+    cursor: pointer !important;
   }
 
   :global(.steps .steps-horizontal .step) {
