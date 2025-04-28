f9fdd7a83b1448625fe27de558bedbc35e15d427
Devlin <tinhocgocong@gmail.com>
2025-04-28 14:52:53 +0700

    refactor: Removed completelly instruction

--- src/data/models/prompt.model.ts
+++ src/data/models/prompt.model.ts
@@ -9,7 +9,6 @@ const PromptSchema = z.object({
   description: z.string(),
   category: z.instanceof(ObjectId).optional(),
   group: z.instanceof(ObjectId).optional(),
-  instructions: z.array(z.instanceof(ObjectId)).optional(),
   knowledgebase: z.array(z.instanceof(ObjectId)).optional(),
   prompt: z.string(),
   predefined_input: z.string().optional(),
@@ -28,7 +27,6 @@ const convertObjectIdToString = (doc: Document<Prompt>) => {
     creator_id: doc.creator_id?.toString(),
     category: doc.category?.toString(),
     group: doc.group?.toString(),
-    instructions: doc.instructions?.map((id) => id.toString()),
     knowledgebase: doc.knowledgebase?.map((id) => id.toString()),
     documents: doc.documents?.map((id) => id.toString()),
   };
