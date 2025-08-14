30a247a1219884d5a0b166fe61fcdb6b7eec4309
Steve Nguyen <hoanghcmus@gmail.com>
2025-08-14 09:30:57 +0700

    feat AINOW-1292 AINOW-1303 [Netlify] [BE] [FE] Handle updating Tenant Setting for selecting gpt-4 or gpt-5 (Allow selecting reason level) - Update radio buttons

--- src/components/RadioButtons.svelte
+++ src/components/RadioButtons.svelte
@@ -14,43 +14,19 @@
 <div class="w-full">
   <div class="mb-2 text-base-content font-medium text-sm">Reasoning Effort</div>
   <div class="flex flex-row space-x-4">
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          name="priority"
-          class="radio"
-          value="low"
-          bind:group={value}
-        />
-        <span class="label-text">low</span>
-      </label>
-    </div>
-
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          name="priority"
-          class="radio"
-          value="medium"
-          bind:group={value}
-        />
-        <span class="label-text">medium</span>
-      </label>
-    </div>
-
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          name="priority"
-          class="radio"
-          value="high"
-          bind:group={value}
-        />
-        <span class="label-text">high</span>
-      </label>
-    </div>
+    {#each options as option}
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            name="radio-options"
+            class="radio"
+            value={option.value}
+            bind:group={value}
+          />
+          <span class="label-text">{option.label}</span>
+        </label>
+      </div>
+    {/each}
   </div>
 </div>
