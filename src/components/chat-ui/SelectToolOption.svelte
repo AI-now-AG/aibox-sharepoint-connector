24c3cd6397dfaf4f3f43ae461cb0e61ff4655b43
Steve Nguyen <hoanghcmus@gmail.com>
2025-08-27 11:09:57 +0700

    feat: AINOW-1131 AINOW-1342 [FE] Handle tools - Support deselect tool option

--- src/components/chat-ui/SelectToolOption.svelte
+++ src/components/chat-ui/SelectToolOption.svelte
@@ -1,7 +1,9 @@
+<!-- svelte-ignore event_directive_deprecated -->
 <script lang="ts">
   import Dropdown, { type Option } from "$components/form/Dropdown.svelte";
   import { useTranslations } from "$i18n/utils";
   import { PromptToolOption } from "$types/AIProvider";
+  import { svgIcons } from "$assets/icons";
 
   const t = useTranslations();
 
@@ -25,13 +27,25 @@
 </script>
 
 {#if Array.isArray(toolOptions) && toolOptions.length > 0}
-  <Dropdown
-    placeholder={t("prompt-execution.prompt-tool.placeholder")}
-    options={toolOptions}
-    {classes}
-    {placeholderClasses}
-    {dropdownBoxClasses}
-    bind:value
-    bind:disabled
-  />
+  <div class="flex items-center">
+    <Dropdown
+      placeholder={t("prompt-execution.prompt-tool.placeholder")}
+      options={toolOptions}
+      {classes}
+      {placeholderClasses}
+      {dropdownBoxClasses}
+      bind:value
+      bind:disabled
+    />
+    {#if !disabled && value && (value as PromptToolOption) !== PromptToolOption.None}
+      <button
+        class="btn btn-ghost h-8 p-0 min-h-0 aspect-square ml-1"
+        on:click|preventDefault={() => {
+          value = PromptToolOption.None;
+        }}
+      >
+        {@html svgIcons.close}</button
+      >
+    {/if}
+  </div>
 {/if}
