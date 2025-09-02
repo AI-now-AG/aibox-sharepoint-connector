f8784ce56451c5b1a0bc8d34d0ba90cc2f14f084
Steve Nguyen <hoanghcmus@gmail.com>
2025-09-02 20:24:45 +0700

    fix: create user failed due to access token required during creating new account

--- src/data/models/user.model.ts
+++ src/data/models/user.model.ts
@@ -40,7 +40,7 @@ const UserSchema = z.object({
   blocked: z.boolean().default(false),
   navState: z.record(z.string(), z.boolean()).optional(),
   tours: z.array(UserTour).optional(),
-  auth0_access_token: z.string(),
+  auth0_access_token: z.string().optional(),
 });
 
 export const UserFilterParamsSchema = z.object({
