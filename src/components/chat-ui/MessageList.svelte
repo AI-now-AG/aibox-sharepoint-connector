299805bd4b97bef355cf82cb8655a9a3f7fd252b
Devlin <tinhocgocong@gmail.com>
2025-10-06 19:17:18 +0700

    fix: Fixed weird conversation title

--- src/components/chat-ui/MessageList.svelte
+++ src/components/chat-ui/MessageList.svelte
@@ -120,7 +120,7 @@
                             <FileAttachmentList {fileUrls} />
                           {/if}
                         </div>
-                      {:else if !imageUrl}
+                      {:else}
                         <div class="flex-1 p-4 pt-2.5">
                           <p class="font-bold text-sm">
                             {role == MessageRole.User ? username : `aibox`}
