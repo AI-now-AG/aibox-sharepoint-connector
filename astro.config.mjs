90622237081b4d10634d796ffb07076a8bc2ef2a
Steve Nguyen <hoanghcmus@gmail.com>
2025-10-07 16:44:10 +0700

    performance: AINOW-1480  - disable source map

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,7 +44,7 @@ export default defineConfig({
       transformer: "lightningcss",
     },
     build: {
-      sourcemap: process.env.BUILD_CONTEXT !== 'deploy-preview',
+      sourcemap: false,
       minify: 'terser'
     },
     plugins: [tailwindcss()],
