1f855cebcf14b499093d349a24f8b2cf436ccee3
Devlin <tinhocgocong@gmail.com>
2025-10-14 20:09:13 +0700

    feat: Add Prompt in Empty Group

---
+++ src/components/prompt-interface/ContentHeader.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { svgIcons } from "$assets/icons";
+  import { preventDefault } from "$utils/common";
+  import AddPromptDialog from "$components/prompt-interface/AddPromptDialog.svelte";
+  import { useTranslations } from "$i18n/utils";
+
+  interface Props {
+    title: string;
+    currentCategoryId: string;
+    currentGroupId: string;
+    isEditable?: boolean;
+  }
+
+  let {
+    title,
+    currentCategoryId,
+    currentGroupId,
+    isEditable = false,
+  }: Props = $props();
+
+  let addPromptDialog: HTMLDialogElement | undefined = $state();
+
+  const t = useTranslations();
+
+  function addPrompt() {
+    addPromptDialog?.showModal();
+  }
+</script>
+
+<div
+  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 justify-between pt-2 mb-2 lg:pt-8"
+>
+  <h1 class="text-4xl font-bold">
+    {title}
+  </h1>
+  <div class="text-right">
+    {#if isEditable}
+      <button
+        class="btn btn-sm btn-outline font-normal grow-0"
+        onclick={preventDefault(addPrompt)}
+      >
+        {@html svgIcons.add}
+        {t("prompt-library.prompts.add")}
+      </button>
+    {/if}
+  </div>
+</div>
+
+<AddPromptDialog
+  bind:addPromptDialog
+  {currentCategoryId}
+  {currentGroupId}
+  {isEditable}
+/>
