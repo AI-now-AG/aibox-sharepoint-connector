c8522cc275e461860e46da415364ea387a985923
Devlin <tinhocgocong@gmail.com>
2025-10-23 11:01:20 +0700

    fix: disable Lucia session auto-extension behavior

--- src/pages/login/auth0/index.ts
+++ src/pages/login/auth0/index.ts
@@ -21,10 +21,10 @@ export async function GET(context: APIContext): Promise<Response> {
 
   // 👇 tell Auth0 which API you want a token for
   // see audience setting in Auth0: Applications > APIs > Azure Backend API
-  authorizeUrl.searchParams.set(
-    "audience",
-    import.meta.env.AUTH0_API_AUDIENCE || "https://dev-api.aibox-app.com",
-  );
+  // authorizeUrl.searchParams.set(
+  //   "audience",
+  //   import.meta.env.AUTH0_API_AUDIENCE || "https://dev-api.aibox-app.com",
+  // );
 
   return context.redirect(authorizeUrl.toString());
 }
