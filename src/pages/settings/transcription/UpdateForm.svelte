675eb295dea92070d6b113a138902dd59a8e469b
Vishal <vishal@ainow.ch>
2025-11-04 21:33:15 +0530

    remove custom formate issues

--- src/pages/settings/transcription/UpdateForm.svelte
+++ src/pages/settings/transcription/UpdateForm.svelte
@@ -9,7 +9,6 @@
   import { preventDefault } from "$utils/common";
   import { normalizeTextToHtml } from "$utils/textFormatting";
   import TextEditor from "$components/form/TextEditor.svelte";
-  import { htmlToMarkdown } from "$utils/textFormatting";
   const t = useTranslations();
 
   interface Props {
@@ -104,7 +103,7 @@
       tenant_id: $tenant!._id.toString(),
       category: category,
       enabled: isEnabled,
-      text: htmlToMarkdown(instructionText),
+      text: instructionText,
     };
 
     const { error } = await actions.transcription.create(transcriptionCreate);
@@ -136,7 +135,7 @@
       tenant_id: $tenant!._id.toString(),
       category: category,
       enabled: isEnabled,
-      text: htmlToMarkdown(instructionText),
+      text: instructionText,
     };
 
     const { error } = await actions.transcription.update(transcriptionUpdate);
