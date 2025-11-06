d41c67fda36dd97ad45ee09360645a53e9b5f9db
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-06 18:05:48 +0700

    feat: AINOW-1544 [FE] Implement POC - Enable Session Recording by default --> Off by condition on the Dashboard: https://eu.posthog.com/project/99283/replay/settings#selectedSetting=replay-triggers

--- src/utils/posthogClient.ts
+++ src/utils/posthogClient.ts
@@ -4,7 +4,7 @@ import posthogClient from 'posthog-js'
 if (typeof window !== 'undefined') {
     posthogClient.init(import.meta.env.PUBLIC_POSTHOG_KEY, {
         api_host: POSTHOG_API_HOST,
-        disable_session_recording: true
+        // disable_session_recording: true
     })
 }
 
