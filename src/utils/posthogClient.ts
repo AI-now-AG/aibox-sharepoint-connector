ebc5bafceb583194f0bcda7cffe8a498fc5d4ce0
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-07 14:04:31 +0700

    feat: AINOW-1544 [FE] Implement POC - Clean up Posthog instance and document

--- src/utils/posthogClient.ts
+++ src/utils/posthogClient.ts
@@ -1,16 +1,52 @@
 import { EventName as AiboxEventName, POSTHOG_API_HOST } from '$types/Posthog'
-import posthogClient, { type CaptureOptions, type EventName, type Properties } from 'posthog-js'
+import posthogClient, { PostHog, type CaptureOptions, type EventName, type Properties } from 'posthog-js'
 
+let clientInstance: PostHog;
+
+/**
+ * This is PostHog instance, capture for SERVER Side 
+ * 
+ * Important Note: DO NOT export `posthogClient` OR `clientInstance` to controll for specific tenant (exlucde all Posthog script + init when Tenant setting Off)
+ * 
+ * EX: `export default posthogClient` OR `export default clientInstance` ==> cause loading during import 
+*/
+export function getPosthogClilentInstance() {
+    if (clientInstance) {
+        return clientInstance;
+    }
+    if (typeof window !== 'undefined') {
+        clientInstance = posthogClient.init(import.meta.env.PUBLIC_POSTHOG_KEY, {
+            api_host: POSTHOG_API_HOST,
+        })
+
+    }
+    return clientInstance;
+}
+
+/**
+   * Captures an event with optional properties
+   *
+   * @example
+   * ```ts
+   * posthogClientCapture(tenant, EventName.AiboxPromptResult, {
+   *    tenant_id: $tenant?._id?.toString() || "-",
+   *    tenant_name: $tenant?.name?.toString() || "-",
+   *    prompt_name: currentPrompt?.title || "-",
+   *    tool: selectedPromptTool,
+   *    model: getModelName($tenant, currentPrompt?.model),
+   *    from: ScreenName.PromptExecutionArea,
+   * })
+   * ```
+   *
+   * @param tenant Check tenant.is_on_posthog to decide identify or not
+   * @param event_name {@link AiboxEventName} 
+   * @param properties {@link Properties}
+   * @param options {@link CaptureOptions}
+   * @returns 
+*/
 export function posthogClientCapture(tenant: { is_on_posthog?: boolean } | null | undefined, event_name: AiboxEventName | EventName, properties?: Properties | null, options?: CaptureOptions) {
     if (!tenant || !tenant.is_on_posthog) {
         return;
     }
-    if (typeof window !== 'undefined') {
-        posthogClient.init(import.meta.env.PUBLIC_POSTHOG_KEY, {
-            api_host: POSTHOG_API_HOST,
-        })
-    }
-    posthogClient.capture(event_name, properties, options);
+    getPosthogClilentInstance?.()?.capture(event_name, properties, options);
 }
-
-export default posthogClient
--- src/utils/posthogServer.ts
+++ src/utils/posthogServer.ts
@@ -2,22 +2,76 @@
 import { POSTHOG_API_HOST } from '$types/Posthog'
 import { PostHog, type EventMessage, type IdentifyMessage } from 'posthog-node'
 
-const posthogServer = new PostHog(import.meta.env.PUBLIC_POSTHOG_KEY, {
-    host: POSTHOG_API_HOST
-})
+let serverInstance: PostHog;
 
+/**
+ * This is PostHog instance, capture for CLIENT Side 
+ * 
+ * Important Note: DO NOT export `serverInstance` to controll for specific tenant (exlucde all Posthog script + init when Tenant setting Off)
+ * 
+ * EX: `export default serverInstance`  ==> cause load during import 
+*/
+export function getPosthogServerInstance() {
+    if (serverInstance) {
+        return serverInstance;
+    }
+    serverInstance = new PostHog(import.meta.env.PUBLIC_POSTHOG_KEY, {
+        host: POSTHOG_API_HOST
+    })
+    return serverInstance;
+}
+
+/**
+   * Captures an event with optional properties
+   *
+   * @example
+   * ```ts
+   * posthogServerCapture(tenant, {
+   *   distinctId: userId?.toString(),
+   *   event: EventName.AiboxLogin,
+   *   properties: {
+   *    username: auth0User.data.nickname,
+   *    name: auth0User.data.name,
+   *    email: auth0User.data.email,
+   *    tenant_id: tenant._id.toString(),
+   *   }
+   * })
+   * ```
+   *
+   * @param tenant Check tenant.is_on_posthog to decide identify or not
+   * @param props {@link EventMessage}
+   * @returns 
+*/
 export function posthogServerCapture(tenant: { is_on_posthog?: boolean } | null | undefined, props: EventMessage) {
     if (!tenant || !tenant.is_on_posthog) {
         return;
     }
-    posthogServer.capture(props)
+    getPosthogServerInstance?.()?.capture(props)
 }
 
+/**
+   * Identify a user and set their properties.
+   *
+   * @example
+   * ```ts
+   * posthogServerIdentify(tenant, {
+   *   distinctId: userId?.toString(),
+   *   properties: {
+   *     username: auth0User.data.nickname,
+   *     name: auth0User.data.name,
+   *     email: auth0User.data.email,
+   *     tenant_id: tenant._id.toString(),
+   *   }
+   * })
+   * ```
+   *
+   * @param tenant Check tenant.is_on_posthog to decide identify or not
+   * @param props {@link IdentifyMessage}
+   * @returns 
+*/
 export function posthogServerIdentify(tenant: { is_on_posthog?: boolean } | null | undefined, props: IdentifyMessage) {
     if (!tenant || !tenant.is_on_posthog) {
         return;
     }
-    posthogServer.identify(props)
+    getPosthogServerInstance?.()?.identify(props)
 }
-
-export default posthogServer
