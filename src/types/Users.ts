c1599be6e28ec05f2cb0ed58fbfe85dfd2a4fee0
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-13 17:20:27 +0700

    feat: AINOW-1559 Update permission

--- src/types/Users.ts
+++ src/types/Users.ts
@@ -9,14 +9,15 @@ export enum Permission {
   UserAll = "user:all",
   SuperUserAll = "super:user:all",
   AdminAll = "admin:all",
-  SuperAll = "super:all",
+  SuperAdminAll = "super:admin:all",
+  SuperAllDeprecated = "super:all",
 }
 
 export const ROLE_PERMISSIONS_MAP = {
   [UserRole.User]: [Permission.UserAll],
   [UserRole.SuperUser]: [Permission.UserAll, Permission.SuperUserAll],
   [UserRole.Admin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll],
-  [UserRole.SuperAdmin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll, Permission.SuperAll,],
+  [UserRole.SuperAdmin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll, Permission.SuperAdminAll, Permission.SuperAllDeprecated,],
 };
 
 export enum TourType {
