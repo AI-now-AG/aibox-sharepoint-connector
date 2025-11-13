02cf599f4ba1173ecde628079a0d94f5b0230696
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-13 13:22:08 +0700

    feat: AINOW-1559 AINOW-1575 [AIBOX] [Nelify] [New] Setup for Supper User role

--- src/types/Users.ts
+++ src/types/Users.ts
@@ -1,23 +1,22 @@
 export enum UserRole {
+  User = "User",
+  SuperUser = "Super User",
   Admin = "Admin",
   SuperAdmin = "Super Admin",
-  User = "User",
 }
 
 export enum Permission {
   UserAll = "user:all",
+  SuperUserAll = "super:user:all",
   AdminAll = "admin:all",
   SuperAll = "super:all",
 }
 
 export const ROLE_PERMISSIONS_MAP = {
   [UserRole.User]: [Permission.UserAll],
-  [UserRole.Admin]: [Permission.UserAll, Permission.AdminAll],
-  [UserRole.SuperAdmin]: [
-    Permission.UserAll,
-    Permission.AdminAll,
-    Permission.SuperAll,
-  ],
+  [UserRole.SuperUser]: [Permission.UserAll, Permission.SuperUserAll],
+  [UserRole.Admin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll],
+  [UserRole.SuperAdmin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll, Permission.SuperAll,],
 };
 
 export enum TourType {
