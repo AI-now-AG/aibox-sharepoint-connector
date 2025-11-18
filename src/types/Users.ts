ba6583cde801921704f44d46a9de825355cf768d
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-18 10:27:48 +0700

    feat: AINOW-1559 AINOW-1574 [Auth0] [New] Setup for Supper User - Create maps for FeaturePermission

--- src/types/Users.ts
+++ src/types/Users.ts
@@ -19,6 +19,36 @@ export const ROLE_PERMISSIONS_MAP = {
   [UserRole.SuperAdmin]: [Permission.UserAll, Permission.SuperUserAll, Permission.AdminAll, Permission.SuperAdminAll],
 };
 
+export enum FeaturePermission {
+  AddAssistantInPromptExecutionArea = "AddAssistantInPromptExecutionArea",
+  EditAssistantInPromptExecutionArea = "EditAssistantInPromptExecutionArea",
+  AddCategoryInCategoryList = "AddCategoryInCategoryList",
+  EditCategoryInCategoryForm = "EditCategoryInCategoryForm",
+  AddKnowledgeBaseInKnowledgeBaseList = "AddKnowledgeBaseInKnowledgeBaseList",
+  EditKnowledgeBaseInKnowledgeBaseList = "EditKnowledgeBaseInKnowledgeBaseList",
+  EditKnowledgeBaseInKnowledgeBaseForm = "EditKnowledgeBaseInKnowledgeBaseForm",
+  ImportExportAddAssistantInAssistantLibrary = "ImportExportAddAssistantInAssistantLibrary",
+  EditAssistantInAssistantLibrary = "EditAssistantInAssistantLibrary",
+  EditInstructionInAudioInstructionList = "EditInstructionInAudioInstructionList",
+  EditSubInstructionInAudioSubInstructionList = "EditSubInstructionInAudioSubInstructionList",
+  EditInstructionInstructionUsecaseForm = "EditInstructionInstructionUsecaseForm",
+}
+
+export const FEATURE_PERMISSIONS_MAP: Record<FeaturePermission, Permission[]> = {
+  [FeaturePermission.AddAssistantInPromptExecutionArea]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditAssistantInPromptExecutionArea]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.AddCategoryInCategoryList]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditCategoryInCategoryForm]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.AddKnowledgeBaseInKnowledgeBaseList]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditKnowledgeBaseInKnowledgeBaseList]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditKnowledgeBaseInKnowledgeBaseForm]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.ImportExportAddAssistantInAssistantLibrary]: [Permission.SuperAdminAll],
+  [FeaturePermission.EditAssistantInAssistantLibrary]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditInstructionInAudioInstructionList]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditSubInstructionInAudioSubInstructionList]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+  [FeaturePermission.EditInstructionInstructionUsecaseForm]: [Permission.SuperAdminAll, Permission.AdminAll, Permission.SuperUserAll],
+};
+
 export enum TourType {
   Onboarding = "Onboarding",
   OnboardingNewTenant = "OnboardingNewTenant",
