5a2fc7feb6c37a31aa07a880b4f39691dac4cdaf
Devlin <tinhocgocong@gmail.com>
2025-11-25 17:38:55 +0700

    feat: Implement new flow for password reset email

--- src/constants.ts
+++ src/constants.ts
@@ -51,13 +51,13 @@ export const FEATURE_MAP_ROUTES = {
 };
 
 export const SKIP_CHEKING_ONBOARDING_ROUTES = [
-  '/subscription',
-  '/subscription/*',
-  '/logout',
-  '/logout/*',
-  '/api/logout',
-  '/_actions/*'
-]
+  "/subscription",
+  "/subscription/*",
+  "/logout",
+  "/logout/*",
+  "/api/logout",
+  "/_actions/*",
+];
 
 // Auth0
 export const AUTH0_SESSION_STATE = "auth0_state";
@@ -76,6 +76,7 @@ export const AUTH0_AUTH_WINDOWS_CON_PROD = "con_Si1JPT62XNjp9gVp";
 
 // SendGrid
 export const SG_VERIFICATION_TEMPLATE = "d-69ed72334042458783f985ada5dbe61d";
+export const SG_PASSWORD_RESET_TEMPLATE = "d-69ed72334042458783f985ada5dbe61d";
 export const SG_NEW_TENANT_TEMPLATE = "d-87847bafc967448e93d99a411edece3a";
 
 // Tenants
