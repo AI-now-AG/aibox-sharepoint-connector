d584300f2d71c2050fb9133d962a2da5bf18aac9
Devlin <tinhocgocong@gmail.com>
2026-01-08 17:02:23 +0700

    Removed weekday

--- src/pages/user-management/UserForm.svelte
+++ src/pages/user-management/UserForm.svelte
@@ -352,7 +352,7 @@
                 <td class="text-base-content/80">{t("user.signed-up")}</td>
                 <td class="text-base">
                   {dayjs(userData.created_at, "DD.MM.YYYY").format(
-                    "dddd DD.MM.YYYY",
+                    "DD.MM.YYYY",
                   )}
                 </td>
               </tr>
