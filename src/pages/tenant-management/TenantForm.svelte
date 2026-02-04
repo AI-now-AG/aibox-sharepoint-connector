e4c3f9a2c9d91a7b4db9b43c58f4fd9e86760bde
Devlin <tinhocgocong@gmail.com>
2026-02-04 19:53:37 +0700

    feat: Enhance tenant managment

--- src/pages/tenant-management/TenantForm.svelte
+++ src/pages/tenant-management/TenantForm.svelte
@@ -1417,6 +1417,15 @@
             {/if}
           </div>
         </div>
+      </div>
+
+      <!-- Contact & Address -->
+      <div class="p-5 mb-8 bg-base-100 rounded-lg">
+        <div class="mb-3 flex flex-row items-center gap-2">
+          {@html svgIcons.money}
+          <p class="font-medium text-md">{"Contact & Address"}</p>
+        </div>
+
         <div class="flex flex-row space-x-4">
           <div class="flex-1 flex flex-col mb-4">
             <span class="mb-2 text-base-content font-medium text-sm"
@@ -1482,31 +1491,6 @@
               class="input input-bordered w-full py-2 text-gray-500"
               bind:value={tenantData.comment}
             ></textarea>
-          </div>
-        </div>
-
-        <div class="flex flex-row space-x-4">
-          <div class="flex-1 flex flex-col mb-4">
-            <span class="mb-2 text-base-content font-medium text-sm"
-              >{t("tenant.user-limit")}</span
-            >
-            <input
-              type="number"
-              placeholder=""
-              class="input input-bordered w-full"
-              bind:value={tenantData.max_user_limit}
-            />
-          </div>
-          <div class="flex-1 flex flex-col mb-4">
-            <span class="mb-2 text-base-content font-medium text-sm"
-              >{t("subscription.number-of-active-user")}</span
-            >
-            <input
-              type="number"
-              class="input input-bordered bg-base-200 w-full"
-              readonly
-              value={activeUsers}
-            />
           </div>
         </div>
       </div>
@@ -3151,11 +3135,11 @@
           </button>
         </div>
 
-        <div class="flex items-center">
+        <div class="flex items-center p-3 bg-base-300/40 rounded-md">
           <input
             id="disable-create-user"
             type="checkbox"
-            class="checkbox checkbox-primary"
+            class="toggle toggle-primary"
             value="disable-create-user"
             bind:checked={tenantData.is_restrict_user_managment}
           />
@@ -3171,11 +3155,11 @@
       <div class="p-5 mb-8 bg-base-100 rounded-lg">
         <h5 class="mb-4 label">{"Status & conditions"}</h5>
         <div class="flex p-2 items-center justify-between">
-          <div class="flex items-center">
+          <div class="flex items-center p-3 bg-base-300/40 rounded-md">
             <input
               id="is-on-posthog"
               type="checkbox"
-              class="checkbox checkbox-primary"
+              class="toggle toggle-primary"
               value="is-on-posthog"
               bind:checked={tenantData.is_on_posthog}
             />
@@ -3184,6 +3168,36 @@
                 >{t("tenant.posthog")}</span
               >
             </label>
+          </div>
+        </div>
+      </div>
+
+      <!-- User Limits -->
+      <div class="p-5 mb-8 bg-base-100 rounded-lg">
+        <h5 class="mb-4 label">{"User Limits"}</h5>
+
+        <div class="flex flex-row space-x-4">
+          <div class="flex-1 flex flex-col mb-4">
+            <span class="mb-2 text-base-content font-medium text-sm"
+              >{t("tenant.user-limit")}</span
+            >
+            <input
+              type="number"
+              placeholder=""
+              class="input input-bordered w-full"
+              bind:value={tenantData.max_user_limit}
+            />
+          </div>
+          <div class="flex-1 flex flex-col mb-4">
+            <span class="mb-2 text-base-content font-medium text-sm"
+              >{t("subscription.number-of-active-user")}</span
+            >
+            <input
+              type="number"
+              class="input input-bordered bg-base-200 w-full"
+              readonly
+              value={activeUsers}
+            />
           </div>
         </div>
       </div>
