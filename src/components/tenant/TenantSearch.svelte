e8cd783097a91a9ff1ace09ff6c7d52b958e3f1e
Devlin <tinhocgocong@gmail.com>
2026-03-04 21:35:28 +0700

    feat: Improve Tenant List in Report

--- src/components/tenant/TenantSearch.svelte
+++ src/components/tenant/TenantSearch.svelte
@@ -115,7 +115,7 @@
 
   {#if filteredTenants.length > 0}
     <ul
-      class="dropdown-content menu shadow bg-base-100 rounded-box w-full max-h-[250px] overflow-scroll"
+      class="dropdown-content menu shadow bg-base-100 rounded-box w-full max-h-[250px] flex-nowrap"
       out:fade
     >
       {#each filteredTenants as option}
