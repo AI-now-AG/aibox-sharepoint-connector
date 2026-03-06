cd2d18fbe956c63c5c5ba818db204e55f56884ee
Devlin <tinhocgocong@gmail.com>
2026-03-06 17:21:43 +0700

    fix: Make all labels in the Tenant Form same size and style

--- src/pages/tenant-management/TenantForm.svelte
+++ src/pages/tenant-management/TenantForm.svelte
@@ -1392,6 +1392,7 @@
               label={`${t("tenant.language")}*`}
               options={Languges}
               bind:value={selectedLanguage}
+              labelClasses="font-medium text-sm"
             />
           </div>
 
@@ -1418,6 +1419,7 @@
               label={t("tenant.subscription")}
               options={subscriptionOptions}
               bind:value={selectedPlan}
+              labelClasses="font-medium text-sm"
             />
           </div>
           <div class="flex-1 flex flex-col mb-4">
@@ -1469,7 +1471,7 @@
 
         <div class="flex flex-row space-x-4">
           <div class="flex-1 flex flex-col mb-4">
-            <p class="mb-2">{t("tenant.total-price")}</p>
+            <p class="mb-2 font-medium text-sm">{t("tenant.total-price")}</p>
             <label class="input input-bordered w-full">
               {@html svgIcons.inputDollarIcon}
               <input
@@ -1484,7 +1486,9 @@
 
         <div class="flex flex-row space-x-4">
           <div class="flex-1 flex flex-col mb-4">
-            <p class="mb-2">{t("tenant.subscription-start-date")}</p>
+            <p class="mb-2 font-medium text-sm">
+              {t("tenant.subscription-start-date")}
+            </p>
             <input
               type="date"
               bind:value={subscriptionData.start_date}
@@ -1492,7 +1496,9 @@
             />
           </div>
           <div class="flex-1 flex flex-col mb-4">
-            <p class="mb-2">{t("tenant.subscription-cancelled-date")}</p>
+            <p class="mb-2 font-medium text-sm">
+              {t("tenant.subscription-cancelled-date")}
+            </p>
 
             <input
               type="date"
@@ -2875,6 +2881,7 @@
               allowClear={true}
               bind:value={tenantData.owned_by_reseller}
               disabled={tenantData.is_reseller}
+              labelClasses="font-medium text-sm"
             />
           </div>
         </div>
