fde137f44e8b2d766841c72f9d9693dc54249299
Devlin <tinhocgocong@gmail.com>
2026-03-11 17:37:58 +0700

    feat: Implement Workflow to Set Up Knowledge Base

--- src/components/tenant/TenantCreationSuccess.svelte
+++ src/components/tenant/TenantCreationSuccess.svelte
@@ -207,9 +207,9 @@
         <div transition:fade class="mt-5">
           <div
             role="alert"
-            class="alert alert-vertical sm:alert-horizontal bg-warning/30 mb-2"
+            class="alert alert-vertical sm:alert-horizontal bg-success/30 mb-2"
           >
-            <div class="text-warning">
+            <div class="text-success">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-6 w-6 shrink-0 stroke-current"
@@ -225,7 +225,7 @@
               </svg>
             </div>
             <div>
-              <h3 class="text-warning-content font-bold">
+              <h3 class="font-bold">
                 {t("reseller.created-tenant.status-sent")}
               </h3>
               <div class="text-xs">
