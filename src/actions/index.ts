75bbd0d0624c281f864db455dba4f8c50c816cef
Devlin <tinhocgocong@gmail.com>
2026-03-11 18:54:33 +0700

    feat: Create Knowledge Base in existing Customer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,7 +4,7 @@ import { category } from "./category";
 import { prompt } from "./prompt";
 import { transcription } from "./transcription";
 import { user } from "./user";
-import { knowledgeBase } from "./knowledgeBase2";
+import { knowledgeBase } from "./knowledgeBase";
 import { conversation } from "./conversation";
 import { onboarding } from "./onboarding";
 import { report } from "./report";
